11fbe07f88d2fbcb3eea5ad3672a212e91c67c30
Blaz Korecic <blaz.korecic@ug.uchile.cl>
2019-10-15 21:35:29 -0300

    Edit Segment Trees

--- datastructures/SegmentTreeLazy.cpp
+++ datastructures/SegmentTreeLazy.cpp
@@ -1,146 +1,81 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 typedef long long ll;
-const ll inf = 1e14;
-struct SegmentTree
-{
-	vector<ll> ST, L;
-	int N;
 
-	SegmentTree(vector<ll> &A)
-	{
-		N = A.size();
-
-		// Recordar 4*N
-		ST.resize(4*N);
-		L.resize(4*N,0);
-
-		build(1,0,N-1,A);
-	}
-
-	void build(int n, int l, int r, vector<ll> &A)
-	{	
-		// Chequeo si es hoja
-		if(l == r)
-		{
-			ST[n] = A[r];
-			return;
-		}
-
-		// Construyo los hijos
-		build(2*n,l,(l+r)/2,A);
-		build(2*n+1,(l+r)/2+1,r,A);
-
-		// Computo los valores
-		ST[n] = min(ST[2*n],ST[2*n+1]);
-	}
-
-	ll query(int i, int j)
-	{
-		return query(1,0,N-1,i,j);
-	}
-
-	// Computa la query(i,j) para el nodo n
-	ll query(int n, int l, int r, int i, int j)
-	{
-		// Si hay que actualizar el nodo
-		if(L[n] != 0)
-		{
-			// Actualizo el valor
-			ST[n] += L[n];
-
-			// Si no es hoja, marco a los hijos
-			if(l != r)
-			{
-				L[2*n] += L[n];
-				L[2*n+1] += L[n];
-			}
-
-			// Desmarco al nodo
-			L[n] = 0;
-		}
-
-		// Caso 1: No chocan
-		if(r < i || j < l) return inf;
-
-		// Caso 2: Contenido totalmente
-		if(i <= l && r <= j) return ST[n];
-
-		// Caso 3: Ninguno de los anteriores
-		// Le paso la query a los hijos
-
-		return min(query(2*n,l,(l+r)/2,i,j),
-					query(2*n+1,(l+r)/2+1,r,i,j));
-	}
-
-	// A[i,j] += v
-	void update(int i, int j, ll v)
-	{
-		return update(1,0,N-1,i,j,v);
-	}
-
-	void update(int n, int l, int r, int i, int j, ll v)
-	{
-		// Si hay que actualizar el nodo
-		if(L[n] != 0)
-		{
-			// Actualizo el valor
-			ST[n] += L[n];
-
-			// Si no es hoja, marco a los hijos
-			if(l != r)
-			{
-				L[2*n] += L[n];
-				L[2*n+1] += L[n];
-			}
-
-			// Desmarco al nodo
-			L[n] = 0;
-		}
-
-		// Caso 1: No chocan
-		if(j < l || r < i) return;
-
-		// Caso 2: Contenido totalmente
-		if(i <= l && r <= j)
-		{
-			// Actualizo el valor
-			ST[n] += v;
-
-			// Si no es hoja, marco a los hijos
-			if(l != r)
-			{
-				L[2*n] += v;
-				L[2*n+1] += v;
-			}
-
-			// Desmarco al nodo
-			L[n] = 0;
-
-			return;
-		}
-
-		// Caso 3: Ninguno de los anteriores
-		// Actualizar a los hijos
-		update(2*n,l,(l+r)/2,i,j,v);
-		update(2*n+1,(l+r)/2+1,r,i,j,v);
-
-		// Recomputo los valores
-		ST[n] = min(ST[2*n],ST[2*n+1]);
-	}
+struct SegmentTree{
+    vector <ll> tree;
+    vector <ll> lazy;
+    int N;
+    SegmentTree(vector <ll> &A){
+        N = A.size();
+        tree.resize(4*N);
+        lazy.assign(4*N, 0);
+        build(0, 0, N-1, A);
+    }
+ 
+    ll merge(ll a, ll b){
+        return a+b;
+    }
+ 
+    void build(int n, int l, int r, vector <ll> &A){
+        if(l == r){
+            tree[n] = A[l];
+            return;
+        }
+        int mid = (l+r)/2;
+        build(2*n+1, l, mid, A);
+        build(2*n+2, mid+1, r, A);
+        tree[n] = merge(tree[2*n+1], tree[2*n+2]);
+    }
+ 
+    ll query(int i, int j){
+        return query(0, 0, N-1, i, j);
+    }
+ 
+    ll query(int n, int l, int r, int i, int j){
+        if(lazy[n] != 0){ // update pendiente
+            tree[n] += lazy[n]*(r-l+1);
+            if(l != r){
+                lazy[2*n+1] += lazy[n];
+                lazy[2*n+2] += lazy[n];
+            }
+            lazy[n] = 0;
+        }
+        if(i <= l && r <= j){ // contenido completamente
+            return tree[n];
+        }
+        if(r < i || j < l){ // fuera del rango
+            return 0;
+        }
+        int mid = (l+r)/2; // contenido parcialmente
+        ll a = query(2*n+1, l, mid, i, j);
+        ll b = query(2*n+2, mid+1, r, i, j);
+        return merge(a,b);
+    }
+ 
+    void update(int i, int j, ll val){
+        return update(0, 0, N-1, i, j, val);
+    }
+ 
+    void update(int n, int l, int r, int i, int j, ll val){
+        if(lazy[n] != 0){ // update pendiente
+            tree[n] += lazy[n]*(r-l+1);
+            if(l != r){
+                lazy[2*n+1] += lazy[n];
+                lazy[2*n+2] += lazy[n];
+            }
+            lazy[n] = 0;
+        }
+        if(r < i || j < l) return; // fuera del rango
+        if(i <= l && r <= j){ // contenido completamente
+            tree[n] += val*(r-l+1);
+            if(l != r){
+                lazy[2*n+1] += val;
+                lazy[2*n+2] += val;
+            }
+            return;
+        }
+        int mid = (l+r)/2; // contenido parcialmente
+        update(2*n+1, l, mid, i, j, val);
+        update(2*n+2, mid+1, r, i, j, val);
+        tree[n] = merge(tree[2*n+1], tree[2*n+2]);
+    }
 };
-
-int main()
-{
-	vector<int> A = {1,2,7,9,2,3,5,1,2,9,8};
-	SegmentTree ST(A);
-
-	// query(2,5) = min([7,9,2,3]) = 2
-	cout << ST.query(2,5) << '\n';
-
-	// query(0,10) = min(A) = 1
-	cout << ST.query(0,10) << '\n';
-
-	return 0;
-}
