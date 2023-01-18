2026c6fd2414ed8ff63f7911ab9baf4bd74d9694
bkorecic <blaz.korecic@ug.uchile.cl>
2023-01-18 17:56:14 -0300

    Improve persistent segment tree

--- data-structures/SegmentTreePersistent.cpp
+++ data-structures/SegmentTreePersistent.cpp
@@ -1,56 +1,53 @@
+// Same time complexity as normal SegmentTree
+// Additional O(log(n)) memory per update
+// Tested on https://cses.fi/problemset/task/1737
+template <typename T, T merge(T, T)>
 struct Node{
-    // Usa log(n) memoria adicional por versión
-    Node *left, *right;
-    int val;
-    Node(int a=0, Node *b=NULL, Node *c=NULL){
-        val = a;
-        left = b;
-        right = c;
-    }
-    void build(int l, int r){
-        if(l == r) return;
-        left = new Node();
-        right = new Node();
-        int mid = (l+r)/2;
-        left -> build(l, mid);
-        right -> build(mid+1, r);
+    Node *left=0, *right=0;
+	int i, j;
+    T val;
+	Node() {}
+	Node(int _i, int _j) : i(_i), j(_j) {}
+	Node(vector <T> &A){
+		int N = int(A.size());
+		i = 0, j = N-1;
+		build(A);
+	}
+
+    void build(vector <ll> &A){
+        if(i == j){
+			val = A[i];
+			return;
+		}
+        int mid = (i+j)/2;
+        left = new Node<T,merge>(i, mid);
+        right = new Node<T,merge>(mid+1, j);
+        left->build(A);
+        right->build(A);
+		val = merge(left->val, right->val);
     }
 
-    Node *update(int l, int r, int i, ll v){
-        if(i < l || r < i){
+    Node *update(int t, ll v){
+        if(t < i || j < t){
             return this;
         }
-        if(l == r){
-            Node *ret = new Node(val, left, right);
-            ret -> val += v;
+        if(i == j){
+            Node *ret = new Node<T,merge>(*this);
+			ret->val = v;
             return ret;
         }
-        int mid = (l+r)/2;
-        Node *ret = new Node(val);
-        ret -> left = left -> update(l, mid, i, v);
-        ret -> right = right -> update(mid+1, r, i, v);
-        ret -> val = ret -> left -> val + ret -> right -> val;
+        Node *ret = new Node<T,merge>(i, j);
+        ret->left = left->update(t, v);
+        ret->right = right->update(t, v);
+        ret->val = merge(ret->left->val, ret->right->val);
         return ret;
     }
 
-    ll query(int l, int r, int i, int j){
-        if(r < i || j < l) return 0;
-        if(i <= l && r <= j) return val;
-        int mid = (l+r)/2;
-        return left -> query(l, mid, i, j) 
-               + right -> query(mid+1,r, i, j);
+    ll query(int l, int r){
+        if(l <= i && j <= r) return val;
+		int mid = (i+j)/2;
+		if(mid < l || r < i) return right->query(l, r);
+		else if(j < l || r < mid+1) return left->query(l, r);
+        return merge(left->query(l, r), right->query(l, r));
     }
 };
-
-int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0);
-	int N = 10;
-    vector <Node*> root;
-    root.emplace_back(new Node());
-    root[0] -> build(0,N-1); // 10 espacios
-    root[0] = root[0] -> update(0, N-1, 0, 10); // update en mismo root
-    root[1] = root[0] -> update(0, N-1, 1, 5); // nueva versión
-	cout << root[0] -> query(0, N-1, 0, 2) << '\n'; // 10
-	cout << root[1] -> query(0, N-1, 0, 2) << '\n'; // 15
-    return 0;
-}
